0bdedcc90377d38cdc459b4985e458cce27170ba
Conglei <shiconglei@gmail.com>
2014-01-09 15:33:07 +0800

    final version of the tutorial

--- server.js
+++ server.js
@@ -1,43 +1,26 @@
 /**
  * User: conglei
- * Date: 17/07/13
+ * Date: 09/01/14
  * Time: 13:51
  */
 
-var fs = require("fs");
 var host = "127.0.0.1";
 var port = 8080;
 var express = require("express");
-var formidable = require("formidable");
 var util = require("util");
 var app = express();
 
-var t = 0;
 
-app.use(express.static(__dirname)); //use static files in ROOT/public folder
+//fill in here
 
 
-app.get("/", function(request, response){ //root dir
-    response.redirect("/index.html")
-});
 
-app.get('/getdata/data', function(req,res){
-    var query = req.query;
-//    console.log(query);
-    var num = query['num'];
-    var output = [];
-    for (var i = 0; i < num; i++){
-        output.push(
-            {
-                time : t++,
-                value : Math.random() * 100
-            }
-        );
-    }
-    res.send(output);
-    res.end();
-});
+
+
+
 
 
 
 app.listen(port, host);
+
+console.log("Server Started: port: 8080.....");
